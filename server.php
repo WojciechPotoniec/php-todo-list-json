d8441790164c4912041840b0c6ae4d01da2ce7ed
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-07 10:05:09 +0200

    update

--- server.php
+++ server.php
@@ -2,3 +2,18 @@
 
 $todoJson = file_get_contents("data.json");
 
+if(isset($_POST["id"])){
+    $list = json_decode($todoJson,true);
+    $newtask = [
+        'id' => $_POST['id'],
+        'text'=> $_POST['text'],
+        'done'=> !(bool)$_POST['done']
+    ];
+    $list[] = $newtask;
+    $todoJson = json_encode($list, JSON_PRETTY_PRINT);
+    file_put_contents('js/data.json', $todoJson);
+}
+$_SERVER['REQUEST_METHOD'];
+
+header("Content-Type: application/json");
+echo $todoJson;
