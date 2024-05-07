1a40738d405f0999a1b0c383a158e4a80b75f974
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-07 18:57:26 +0200

    update method DELETE

--- server.php
+++ server.php
@@ -2,19 +2,28 @@
 
 $todoJson = file_get_contents("js/data.json");
 
-
-if(isset($_POST["id"])){
-    $list = json_decode($todoJson,true);
-    $newtask = [
-        'id' => $_POST['id'],
-        'text'=> $_POST['text'],
-        'done'=> !(bool)$_POST['done']
-    ];
-    $list[] = $newtask;
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method !== 'GET') {
+    if ($method === 'POST') {
+        $list = json_decode($todoJson, true);
+        if (isset($_POST["id"])) {
+            $newtask = [
+                'id' => (int)$_POST['id'],
+                'text' => $_POST['text'],
+                'done' => !(bool) $_POST['done']
+            ];
+            $list[] = $newtask;
+        }
+    }
+    $todoJson = json_encode($list, JSON_PRETTY_PRINT);
+    file_put_contents('js/data.json', $todoJson);
+} elseif ($method === 'DELETE') {
+    $list = json_decode($todoJson, true);
+    $obj = json_decode(file_get_contents('php://input'), true);
+    array_splice($list, $obj['id'], 1);
     $todoJson = json_encode($list, JSON_PRETTY_PRINT);
     file_put_contents('js/data.json', $todoJson);
 }
-$_SERVER['REQUEST_METHOD'];
 
 header("Content-Type: application/json");
 echo $todoJson;
